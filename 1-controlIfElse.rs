e730ac7de0daf9b4dc8787d41d6da3015d097932
WinHtut <winhtutonline@gmail.com>
2023-02-13 09:12:47 +0630

    Create 1-controlIfElse.rs

---
+++ 1-controlIfElse.rs
@@ -0,0 +1,12 @@
+fn main(){
+    let data = 10;
+    let unknow_data = 5;
+
+    if data < unknow_data{
+        println!("data was small {data}");
+    }else {
+        println!("data was big {data}");
+    }
+
+}
+
